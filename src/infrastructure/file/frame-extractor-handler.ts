3aab179bc4fedce7ba0011425f39b9047bfd42e7
Daniel Alves <daniel_aguiar_alves@hotmail.com>
2025-04-21 23:09:53 -0300

    chore: add duration logs (#19)

--- src/infrastructure/file/frame-extractor-handler.ts
+++ src/infrastructure/file/frame-extractor-handler.ts
@@ -5,16 +5,21 @@ import * as os from 'os';
 import * as path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 
-import * as fs1 from 'fs';
+import { execSync } from 'child_process';
 
-console.log('Checking otp path:', fs1.existsSync('/opt/'));
-console.log('Checking node path:', fs1.existsSync('/opt/nodejs/'));
-console.log('Checking bin path:', fs1.existsSync('/opt/nodejs/bin/'));
-console.log('Checking ffmpeg path:', fs1.existsSync('/opt/nodejs/bin/ffmpeg'));
-console.log(
-  'Checking ffprobe path:',
-  fs1.existsSync('/opt/nodejs/bin/ffprobe'),
-);
+try {
+  const output = execSync('/opt/nodejs/bin/ffmpeg -version').toString();
+  console.log('ffmpeg is executable:', output.split('\n')[0]);
+} catch (error) {
+  console.error('Error executing ffmpeg:', error);
+}
+
+try {
+  const output = execSync('/opt/nodejs/bin/ffprobe -version').toString();
+  console.log('ffprobe is executable:', output.split('\n')[0]);
+} catch (error) {
+  console.error('Error executing ffprobe:', error);
+}
 
 // Configure o caminho para os binários do FFmpeg
 Ffmpeg.setFfmpegPath('/opt/nodejs/bin/ffmpeg');
@@ -83,7 +88,10 @@ export class FrameExtractorHandler {
   private getVideoDuration(filePath: string): Promise<number> {
     return new Promise((resolve, reject) => {
       Ffmpeg.ffprobe(filePath, (err, metadata) => {
-        if (err) return reject(err);
+        if (err) {
+          console.error('FFPROBE ERROR:', err);
+          return reject(err);
+        }
         resolve(metadata.format.duration ?? 0);
       });
     });
